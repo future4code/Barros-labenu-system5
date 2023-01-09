11e32cbadbd6286bb2f1ede7bbc11af72dddd939
Juliano César Pereira Manzano <julianomanzano@gmail.com>
2023-01-08 21:20:16 -0400

    Teste

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,13 +1,18 @@
-import knex from "knex";
-import dotenv from 'dotenv'
+import knex from "knex"
+import dotenv from "dotenv"
 
-export const connection = knex({
-	client: "mysql",
-	connection: {
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME
-}
+dotenv.config()
+
+const connection = knex({
+   client: "mysql",
+   connection: {
+      host: process.env.DB_HOST,
+      port: 3306,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      multipleStatements: true
+   },
 });
+
+export default connection
